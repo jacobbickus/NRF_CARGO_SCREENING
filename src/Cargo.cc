1b9cf4b3902cd1d934bf150358e83f5816945105
jacobbickus <jbickus@mit.edu>
2021-04-06 15:52:19 -0400

    try this

--- src/Cargo.cc
+++ src/Cargo.cc
@@ -28,7 +28,7 @@ Cargo::Cargo()
 :
 RemoveContainer(false),
 IntObj_rad(4.5*cm), intObjDensity(19.1*g/cm3), IntObj_Selection("Uranium"),
-intObj_radio_abundance(0), cargo_spheres(0), cargo_boxes(0), checkOverlaps(true),
+intObj_radio_abundance(0), cargo_spheres(0), cargo_boxes(0),
 cargoM(NULL)
 {
   cargoM = new CargoMessenger(this);
