26866d600b821b5b0a3cee9ee97beede6d9bc0f5
jacobbickus <jbickus@mit.edu>
2021-04-05 20:03:26 -0400

    fixed back

--- Run_Analysis/MantisROOT.cc
+++ Run_Analysis/MantisROOT.cc
@@ -44,7 +44,7 @@ public:
     void Integral(std::vector<TTree*>);
     void Integral(std::vector<TTree*>,TCut);
     void PredictThickness(std::vector<string>, bool write2file=false);
-    std::vector<std::vector<TH1D*>> PredictThickness(std::vector<string>, double, bool write2file=false);
+    void PredictThickness(std::vector<string>, double, bool write2file=false);
     void RebinHisto(vector<string>, vector<string>, vector<string>, int, double, double);
     void RebinHisto(vector<string>, vector<string>, vector<string>, int, double, double, TCut);
     void VarRebin(vector<string>, vector<string>, vector<string>, int, double, double, TCut, double, double);
@@ -194,7 +194,7 @@ private:
     void ZTest(const char*, const char*, const char*, bool weighted=false);
     void Rebin(bool,const char*,const char*,const char*,int,double Emin=0.0,double Emax=2.1,TCut cut1="NA",bool VarArray=false,double nrf_bin_width=-1.,double non_nrf_bin_width=-1.);
     void Rebin(const char*, const char*, const char*);
-    std::vector<TH1D*> Rescale(const char*, double, bool write2file=false);
+    void Rescale(const char*, double, bool write2file=false);
     void Rescale(const char*, bool write2file=false);
 
     double ReturnBremMax(const char*);
@@ -1000,19 +1000,12 @@ void MantisROOT::PredictThickness(std::vector<string> obj, bool write2file=false
   std::cout << "Thickness Prediction Analysis Complete." << std::endl;
 }
 
-std::vector<std::vector<TH1D*>> MantisROOT::PredictThickness(std::vector<string> obj, double Er, bool write2file=false)
+void MantisROOT::PredictThickness(std::vector<string> obj, double Er, bool write2file=false)
 {
-  std::vector<TH1D*> histov;
-  std::vector<std::vector<TH1D*>> histov_final;
-
   for(int i=0;i<obj.size();++i)
-  {
-    histov = Rescale(obj[i].c_str(),Er, write2file);
-    histov_final.push_back(histov);
-  }
+    Rescale(obj[i].c_str(),Er, write2file);
 
   std::cout << "Thickness Prediction Analysis Complete." << std::endl;
-  return histov_final;
 }
 
 void MantisROOT::RebinHisto(std::vector<string> inFile, std::vector<string> ObjName,
@@ -1981,7 +1974,7 @@ void MantisROOT::ZTest(const char* file1, const char* file2, const char* inObj, 
 
 } // end of ZTest functions
 
-std::vector<TH1D*> MantisROOT::Rescale(const char* inObj, double Er, bool write2file=false)
+void MantisROOT::Rescale(const char* inObj, double Er, bool write2file=false)
 {
   std::cout << "Input the Scale Base Filename:" << std::endl;
   string filename;
@@ -2200,7 +2193,6 @@ std::vector<TH1D*> MantisROOT::Rescale(const char* inObj, double Er, bool write2
 
     std::cout << "Histograms saved." << std::endl;
   }
-  return hOut;
 
 } // end of function
 
@@ -3935,7 +3927,7 @@ void MantisROOT::Show_Integral_Description()
 void MantisROOT::Show_PredictThickness()
 {
   std::cout << "void PredictThickness(std::vector<string> ObjectNames, bool write2file=false)" << std::endl
-  << "vector<vector<TH1D*>> PredictThickness(std::vector<string> ObjectNames, double ResonanceEnergy, bool write2file=false)" << std::endl;
+  << "void PredictThickness(std::vector<string> ObjectNames, double ResonanceEnergy, bool write2file=false)" << std::endl;
 }
 
 void MantisROOT::Show_PredictThickness_Description()
@@ -3944,7 +3936,7 @@ void MantisROOT::Show_PredictThickness_Description()
   << std::endl << "If the second input is passed the thickness calculations will focus on the given resonance energy."
   << std::endl << "If the third input is set to true the scaled histograms will be written to a file."
   << std::endl << "If a Resonance Energy is provided a vector of the scaled histograms is also returned at function call."
-  << std::endl << "Example: std::vector<std::vector<TH1D*>> histov = mantis->PredictThickness({\"IntObjIn\",\"IntObjOut\"},1.73354, true)"
+  << std::endl << "Example: mantis->PredictThickness({\"IntObjIn\",\"IntObjOut\"},1.73354, true)"
   << std::endl << "This would predict thickness effects of IntObjIn and IntObjOut for the 1.73354 resonance energy and write the results to a file."
   << std::endl;
 }
