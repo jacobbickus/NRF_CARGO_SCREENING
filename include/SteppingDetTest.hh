888c136f0294e594291294f22b65c26a1712558e
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2021-04-16 09:51:50 -0400

    Update SteppingDetTest.hh

--- include/SteppingDetTest.hh
+++ include/SteppingDetTest.hh
@@ -63,8 +63,8 @@ public:
   // method from the base class
   virtual void UserSteppingAction(const G4Step*);
 private:
-  G4bool WEIGHTED;
   EventAction* kevent;
+  G4bool WEIGHTED;
   G4OpBoundaryProcessStatus fExpectedNextStatus;
   G4double weight;
 
