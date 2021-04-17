efe0920e90e1e652fc7711a8d7efb8cc8c3c2c46
jacobbickus <jbickus@mit.edu>
2021-04-17 11:43:15 -0400

    try this

--- mantis.cc
+++ mantis.cc
@@ -465,9 +465,12 @@ int main(int argc,char **argv)
 #endif
 
   G4int stop_time = time(0);
-  G4cout << G4endl << "----------------------------------------------------------------------" << G4endl;
-  G4cout << G4endl << "----------------------------------------------------------------------" << G4endl;
-  G4cout << G4endl << "The MC took: [" << ((int)std::difftime(stop_time,start_time))/3600
+  G4cout << G4endl
+  << "----------------------------------------------------------------------" 
+  << G4endl << G4endl
+  << "----------------------------------------------------------------------"
+  << G4endl << G4endl
+  << "The MC took: [" << ((int)std::difftime(stop_time,start_time))/3600
           << " h " << ((int)std::difftime(stop_time,start_time)%3600)/60 << " m "
           << ((int)std::difftime(stop_time,start_time)%3600)%60 << " s]"
           << G4endl << G4endl;
