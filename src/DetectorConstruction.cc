41cc4b7ae164a4c0179b1ed1d70137a76a625ad6
jacobbickus <jbickus@mit.edu>
2021-04-30 08:26:47 -0400

    try this

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -237,10 +237,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
   DetectorInformation* detInfo = DetectorInformation::Instance();
   G4double bremStartPos = detInfo->GetShiftFactor()/(cm);
   detInfo->setBremStartPosition(bremStartPos);
-  G4double container_z_pos = 1.2192*m + 1.5*m - 135.9*cm - std::abs(bremStartPos)*cm;
-
-  if(run_without_chopper)
-    container_z_pos = 0.;
+  G4double container_z_pos = 0;
 
   detInfo->setContainerZPosition(container_z_pos);
   G4double container_edge_position = container_z_pos - 1.2192*m;
